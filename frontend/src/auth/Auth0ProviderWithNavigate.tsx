300626aa27dfbaf33afbcda9c6a4c3eefc33374a
zhengsq23 <2270833979@qq.com>
2024-04-18 23:11:37 +0800

    创建user请求(frontend)

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
@@ -8,6 +9,8 @@ type Props = {
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const navigate = useNavigate();
 
+  const { createUser  } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -17,7 +20,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("user", user);
+    if (user?.sub && user?.email ) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
