20a3bb2e8ca4195be6c6846722b0a965ccce124b
zhengsq23 <2270833979@qq.com>
2024-04-18 17:07:10 +0800

    backend, frontend, database setup

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, {Request,Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
+    console.log("Connected to database!")
+})
+
+
+// 新建express服务器
+const app = express();
+// 添加中间件，将api请求转为json
+app.use(express.json())
+app.use(cors())
+
+// app收到/test请求返回json对象
+app.get("/test",async (req:Request, res:Response)=>{
+    res.json({message:"Hello!"});
+})
+
+// 服务器开启成功执行回调函数
+app.listen(7001,()=>{
+    console.log("server started on localhost:7001")
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <Button>click</Button>
+  )
+}
+
+export default App
