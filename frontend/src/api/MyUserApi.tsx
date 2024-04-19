9e4d6110ba361248fcf2f9d0eef5413f6ba06809
zhengsq23 <2270833979@qq.com>
2024-04-19 15:02:37 +0800

    sonner toast通知栏

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,7 +1,7 @@
 // import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -81,14 +81,14 @@ export const useUpdateMyUser = () => {
     reset,
   } = useMutation(updateMyUserRequest);
 
-  // if (isSuccess) {
-  //   toast.success("User profile updated!");
-  // }
+  if (isSuccess) {
+    toast.success("User profile updated!");
+  }
 
-  // if (error) {
-  //   toast.error(error.toString());
-  //   reset();
-  // }
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
 
   return { updateUser, isLoading };
 };
